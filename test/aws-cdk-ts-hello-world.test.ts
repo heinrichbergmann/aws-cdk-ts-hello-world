432df89ef920f66722200e0c44099634a6f88428
Heinrich Bergmann <heinrich.bergmann@gmail.com>
2022-07-13 11:14:00 +0200

    initial commit

---
+++ test/aws-cdk-ts-hello-world.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsCdkTsHelloWorld from '../lib/aws-cdk-ts-hello-world-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-cdk-ts-hello-world-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsCdkTsHelloWorld.AwsCdkTsHelloWorldStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
